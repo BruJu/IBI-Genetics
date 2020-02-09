001a89fdd4d564b0aeddf3a38c3ec9a616a0753b
Bruju <bruyat.ju@live.fr>
2020-02-09 17:21:58 +0100

    end refactor of base code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 # == Password search problem parameters
 
 # ID of our group
-GROUP_ID = 334
+GROUP_ID = 34
 # List of possibles characters
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 # Minimal size of the password
@@ -20,7 +20,7 @@ MAX_SIZE = 18
 # Size of the population
 SIZE_OF_POPULATION = 100
 # Mutation rate
-MUTATION_RATE = 1
+MUTATION_RATE = 0.01
 # Elitism (number of best individual kept)
 ELITISM = 10
 
@@ -48,6 +48,7 @@ def check(password_attempt):
 
 # =================================================================================================
 # ==== Local Mutations
+
 
 class ILocalMutation:
     """
@@ -180,14 +181,17 @@ LOCAL_MUTATIONS = [
     MutationSwap(1,1), MutationSwap(1,5)
     ]
 
-LOCAL_MUTATIONS_WEIGHTS = [ 2, 2, 1, 2, 1, 3 ]
+LOCAL_MUTATIONS_WEIGHTS = [ 1, 1, 4, 2, 6, 6 ]
 
 
 # =================================================================================================
-# ==== Population manipulation
+# ==== Individual manipulation
 
 
 class Individual:
+    """
+    Represents an individual (a member of the population)
+    """
     def __init__(self, cloned_from=None, crossed_with=None):
         """
         Create a new individual
@@ -207,16 +211,19 @@ class Individual:
             # Note generation number
             self.generation = 0
             self.score = None
+            self.has_already_killed = False # Some individuals can kill others
         elif crossed_with is None:
             self.word = cloned_from.word[:]
             self.generation = cloned_from.generation + 1
             self.score = cloned_from.score
+            self.has_already_killed = cloned_from.has_already_killed
         else:
             min_size = min(len(cloned_from.word), len(crossed_with.word))
             cut_point = random.randint(1, min_size - 1)
-            self.word = cloned_from[0:cut_point] + crossed_with[cut_point:]
+            self.word = cloned_from.word[0:cut_point] + crossed_with.word[cut_point:]
             self.generation = 0
             self.score = None
+            self.has_already_killed = False
 
     def get_score(self):
         """
@@ -229,6 +236,9 @@ class Individual:
         return self.score
     
     def apply_mutation(self, local_mutation_rate=MUTATION_RATE, global_mutation_rate=MUTATION_RATE):
+        """
+        Mutate this individual
+        """
         has_changed = False
 
         if random.random() < global_mutation_rate:
@@ -240,7 +250,7 @@ class Individual:
             if random.random() < local_mutation_rate:
                 has_changed = True
 
-                mutation_func = random.choices(LOCAL_MUTATIONS, LOCAL_MUTATIONS_WEIGHTS)
+                mutation_func = random.choices(LOCAL_MUTATIONS, LOCAL_MUTATIONS_WEIGHTS)[0]
                 current_letter = mutation_func.mutate(self, current_letter)
             else:
                 current_letter = current_letter + 1
@@ -248,11 +258,114 @@ class Individual:
         if has_changed:
             self.generation = 0
             self.score = None
+            self.has_already_killed = False
     
     def global_mutation_shift(self):
         """
         Shift by one letter to the left the letters of the word
         """
-        self.word = self.word[1:].append(self.word[0])
+        self.word = self.word[1:]
+        self.word.append(self.word[0])
+    
+    def to_string(self):
+        return "<{0}> ; Score = {1:.4f} ; Age = {2}".format("".join(self.word), self.get_score(), self.generation)
+    
+    def word_to_str(self):
+        return "".join(self.word)
+    
+    def equals_to_str(self, compared_str):
+        return self.word_to_str == compared_str
+
+def _print_individual():
+    """
+    Basic visual testing of the Individual class
+    """
+
+    ind_a = Individual()
+    print(ind_a.to_string())
+    ind_b = Individual()
+    print(ind_b.to_string())
+
+    clone_of_a = Individual(ind_a)
+    print(clone_of_a.to_string())
+    crossed = Individual(ind_a, ind_b)
+    print(crossed.to_string())
+
+    ind_a.apply_mutation(local_mutation_rate=0.2, global_mutation_rate=0)
+    print(ind_a.to_string())
+
+    ind_b.apply_mutation(local_mutation_rate=0, global_mutation_rate=1)
+    print(ind_b.to_string())
 
 
+
+# =================================================================================================
+# ==== Population manipulation
+
+
+class Population:
+    def __init__(self):
+        self.individuals = []
+        self.generation_number = 0
+        self.last_generation = []
+        self.kill_point = 20
+        self.best_score = 0
+
+    def generate_new_members(self):
+        number_of_iteration = SIZE_OF_POPULATION - len(self.individuals)
+        for _ in range(number_of_iteration):
+            self.individuals.append(Individual())
+
+        self.sort_members()
+
+    def sort_members(self):
+        self.individuals.sort(key=lambda i: i.get_score(), reverse=True)
+        return self.individuals[0].get_score() == 1
+    
+    def generate_next_generation(self, verbose=False):
+        old_population = self.individuals
+
+        self.individuals = []
+        old_population = old_population[0:ELITISM]
+        evaluation = [SIZE_OF_POPULATION - x for x in range(len(old_population))]
+
+        # Elite cloning
+        self.individuals = [Individual(ind) for ind in old_population[0:ELITISM]]
+
+        # Filling with crossover
+        while len(self.individuals) < SIZE_OF_POPULATION:
+            picked_words = random.choices(old_population, weights=evaluation, k=2)
+            self.individuals.append(Individual(picked_words[0], picked_words[1]))
+        
+        # Mutation
+        for individual in self.individuals:
+            individual.apply_mutation(local_mutation_rate=MUTATION_RATE, global_mutation_rate=0)
+
+        self.generation_number = self.generation_number + 1
+
+        r = self.sort_members()
+
+        if verbose:
+            new_best_score = self.individuals[0].get_score()
+            if self.best_score < new_best_score:
+                self.best_score = new_best_score
+                print("Generation {0} : {1}".format(self.generation_number, self.individuals[0].to_string()))
+
+        return r
+        
+
+
+
+# =================================================================================================
+# ==== Password finding
+
+
+def find_password():
+    population = Population()
+    population.generate_new_members()
+
+    while not population.generate_next_generation(verbose=True):
+        pass
+
+
+find_password()
